38911738606512933fd288aeacb2a0c217496d39
xklzv <nikitamelnik061004@gmail.com>
2026-02-12 20:12:00 +0300

    Add support for Android 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.datamatrixscan"
-        minSdk = 31
+        minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
